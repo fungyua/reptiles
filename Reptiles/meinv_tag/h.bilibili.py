18bd930572358e84a6f5fb362c22882bbfa97d90
FungYua <fungyua@gmail.com>
2020-12-13 16:53:28 +0800

    init Reptiles

--- Reptiles/meinv_tag/h.bilibili.py
+++ Reptiles/meinv_tag/h.bilibili.py
@@ -5,22 +5,22 @@ import requests
 import time
 
 
-def get_img(root, Type, item, args, page, minTime=0, maxTime=1):
+def get_img(root, image_type, item, args, page, minTime=0, maxTime=1):
     url = 'https://api.vc.bilibili.com/link_draw/v2/'
     for x in range(page):
-        data = json.loads(requests.get(f'{url}{Type}/list?category={item}&{args}&page_num={x}').text)['data']['items']
+        data = json.loads(requests.get(f'{url}{image_type}/list?category={item}&{args}&page_num={x}').text)['data']['items']
         for value in data:
-            userName = value['user']['name'].replace("|", "_").replace('.', '_')
-            imageName = value['item']['title'].replace("|", "_").replace('.', '_')
-            os.makedirs(f'{root}/{Type}/{item}/{userName}/{imageName}', 775, True)
-            with open(f'{root}/{Type}/{item}/{userName}/_{userName}.jpg', 'wb') as f:
+            user_name = value['user']['name'].replace("|", "_").replace('.', '_')
+            image_name = value['item']['title'].replace("|", "_").replace('.', '_')
+            os.makedirs(f'{root}/{image_type}/{item}/{user_name}/{image_name}', 775, True)
+            with open(f'{root}/{image_type}/{item}/{user_name}/_{user_name}.jpg', 'wb') as f:
                 f.write(requests.get(value['user']['head_url']).content)
-                print('成功下载%s的头像' % userName)
+                print('成功下载%s的头像' % user_name)
             for o, m in enumerate(value['item']['pictures']):
-                print(f'{root}/{Type}/{item}/{userName}/{imageName}/{o + 1}.jpg')
-                with open(f'{root}/{Type}/{item}/{userName}/{imageName}/{o + 1}.jpg', 'wb') as f:
+                print(f'{root}/{image_type}/{item}/{user_name}/{image_name}/{o + 1}.jpg')
+                with open(f'{root}/{image_type}/{item}/{user_name}/{image_name}/{o + 1}.jpg', 'wb') as f:
                     f.write(requests.get(m['img_src']).content)
-                    print('成功下载%s的第%d张图' % (imageName, o + 1))
+                    print('成功下载%s的第%d张图' % (image_name, o + 1))
                     time.sleep(random.randint(minTime, maxTime))
 
 
