75f0d062a54eb8a3bb2cd25346e9e3421f6ed053
FungYua <fungyua@gmail.com>
2020-12-20 13:02:10 +0800

    init Reptiles

--- Reptiles/zhihu_hot.py
+++ Reptiles/zhihu_hot.py
@@ -4,50 +4,41 @@ import re
 import pymongo
 import requests
 
-from config.config import mongo
+from config.config import mongo, headers
 
 
-# from openpyxl import writer, workbook
+class zhihu_hot:
+    def __init__(self):
+        self.url = 'https://www.zhihu.com/billboard'
+        collection_name = 'zhihu'
+        self.db = pymongo.MongoClient(
+            "mongodb://%s:%s@%s:%s" % (
+                mongo.get('user'), mongo.get('password'), mongo.get('host'),
+                mongo.get('port')))[mongo.get('database')]
+        self.collection = self.db[collection_name]
+        if collection_name in self.db.list_collection_names():
+            print('集合已经存在')
+            self.collection.drop()
+            print('集合已经删除')
 
-
-def main():
-    topics = []
-    result = json.loads(
-        re.search(r'(?<="hotList":).+?]', requests.get(url, headers=headers).text).group())
-    i = 1
-    for x in result:
-        target = x['target']
-        topic = {
-            '_id': i,
-            'title': target['titleArea']['text'],
-            'image': target['imageArea']['url'],
-            'hot': target['metricsArea']['text'],
-            'url': target['link']['url'],
-            'desc': target['excerptArea']['text']
-        }
-        i = i + 1
-        topics.append(topic)
-    return topics
+    def run(self):
+        result = json.loads(
+            re.search(r'(?<="hotList":).+?]', requests.get(self.url, headers=headers).text).group())
+        i = 1
+        for x in result:
+            target = x['target']
+            topic = {
+                '_id': i,
+                'title': target['titleArea']['text'],
+                'image': target['imageArea']['url'],
+                'hot': target['metricsArea']['text'],
+                'url': target['link']['url'],
+                'desc': target['excerptArea']['text']
+            }
+            i += 1
+            self.collection.insert_one(topic)
+        print('爬取完毕')
 
 
 if __name__ == '__main__':
-    url = 'https://www.zhihu.com/billboard'
-    collectionName = 'zhihu'
-
-    client = pymongo.MongoClient(
-        "mongodb://%s:%s@%s:%s" % (
-            mongo.get('user'), mongo.get('password'), mongo.get('host'),
-            mongo.get('port')))
-    db = client[mongo.get('database')]
-    collection = db[collectionName]
-
-    headers = {
-        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
-                      'Chrome/87.0.4280.67 Safari/537.36 Edg/87.0.664.47 '
-    }
-    if collectionName in db.list_collection_names():
-        print('集合已经存在')
-        collection.drop()
-        print('集合已经删除')
-    collection.insert_many(main())
-    print('爬取完毕')
+    zhihu_hot().run()
